1c6d7e145f22872a0e5a2e1aaaab62d701fee287
Godsway Kofi Osei <gkosei13@st.knust.edu.gh>
2026-07-23 14:28:55 +0000

    Added .env file

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAnalytics, isSupported } from 'firebase/analytics';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrs3P3PmqG5vpeMea3iigKUvl1Q8d8Tck",
-  authDomain: "visitghana-7ea8f.firebaseapp.com",
-  projectId: "visitghana-7ea8f",
-  storageBucket: "visitghana-7ea8f.firebasestorage.app",
-  messagingSenderId: "1019967000233",
-  appId: "1:1019967000233:web:41e59f4c0ee6a397fee3b8",
-  measurementId: "G-4JHQ964YX8"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,3 @@
-// Change this ONE line to switch between local and cloud
-const BASE_URL = 'https://visitghana-api.onrender.com/api';
-
-// For local testing, comment the line above and uncomment this:
-// const BASE_URL = 'http://192.168.100.4:8081/api';
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'https://visitghana-api.onrender.com/api';
 
 export default BASE_URL;
